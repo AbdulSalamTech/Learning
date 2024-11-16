8d8e5a5965e094ab7cb26589eb2203f744488fc9
abdulsalam <abdulsalam.network@gmail.com>
2024-11-16 15:13:38 +0500

    Update

--- hello.py
+++ hello.py
@@ -10,3 +10,17 @@ if age > 18:
     print(f"{name}'s {age} is greater than 18")
 else:
     print(f"{name}'s age is {age}")
+
+my_name = "AbdulSalam"
+
+print(my_name[5])
+
+names = ["alex", "jhon", "david"]
+
+names.append("harry")
+names.sort()
+
+print(names[0])
+print(names[1])
+
+cordinate = (12, 34)
