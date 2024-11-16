bb36d7a64942c621e32b43989fecc9e51f219ef6
abdulsalam <abdulsalam.network@gmail.com>
2024-11-16 14:09:45 +0500

    Updated

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+name = input("Enter your name: ")
+
+print(f"hello, {name}")
+
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print(f"{name}'s {age} is greater than 18")
+else:
+    print(f"{name}'s age is {age}")
